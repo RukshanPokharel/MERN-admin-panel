01981547a70ba5146dd1bc148a9b851547e2ff2f
Jannick Pedersen <jannickpedersen@live.dk>
2023-06-14 13:53:09 +0200

    Reverted video back after 3 tries

--- client/src/components/Forside/Video.js
+++ client/src/components/Forside/Video.js
@@ -2,26 +2,18 @@ import React from 'react';
 import { Ratio } from 'react-bootstrap';
 
 import '../../styles/Home.css';
-import ReactPlayer from 'react-player';
 
 
 //controls width="100%"
 
+const src = "https://nanoscalesimulations.com/Gallery/BIG-MAP%20demo%201.1.mov";
 
 const Video = () => {
 
     return (
         <div className='video-wrapper' style={{}}>
             <Ratio aspectRatio="16x9">
-                <ReactPlayer  
-                url = 'https://nanoscalesimulations.com/Gallery/BIG-MAP%20demo%201.1.mov'
-                playing={true}
-                loop={true}
-                muted={true}
-                controls={false}
-                id="background_video"
-                aspectRatio="16x9"
-                />
+                <video src={src} type="video/mp4" autoPlay loop muted/>
             </Ratio>
         </div>
     )
