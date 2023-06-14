26035ffe01dbc3193532e15d2cdf6339394c36d0
Jannick Pedersen <jannickpedersen@live.dk>
2023-06-14 13:23:02 +0200

    process.env on video src

--- client/src/components/Forside/Video.js
+++ client/src/components/Forside/Video.js
@@ -6,14 +6,13 @@ import '../../styles/Home.css';
 
 //controls width="100%"
 
-const src = require("https://nanoscalesimulations.com/Gallery/BIG-MAP%20demo%201.1.mov");
 
 const Video = () => {
 
     return (
         <div className='video-wrapper' style={{}}>
             <Ratio aspectRatio="16x9">
-                <video src={src} type="video/mp4" autoPlay loop muted/>
+                <video src={`${process.env.PUBLIC_URL}https://nanoscalesimulations.com/Gallery/BIG-MAP%20demo%201.1.mov`} type="video/mp4" autoPlay loop muted/>
             </Ratio>
         </div>
     )
