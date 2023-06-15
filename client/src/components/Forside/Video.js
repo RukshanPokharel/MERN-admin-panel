c19c037a38d38b6c7d6972f664a9ce1d37a26a5f
Jannick Pedersen <jannickpedersen@live.dk>
2023-06-15 15:04:48 +0200

    fixed video for production build

--- client/src/components/Forside/Video.js
+++ client/src/components/Forside/Video.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React from "react";
 import { Ratio } from 'react-bootstrap';
+import ReactPlayer from 'react-player';
+import myVideo from './MegaGrant.mp4';
 
 import '../../styles/Home.css';
 
@@ -8,13 +10,27 @@ import '../../styles/Home.css';
 
 const src = "https://nanoscalesimulations.com/Gallery/BIG-MAP%20demo%201.1.mov";
 
+// const yt = "https://www.youtube.com/embed/Yo3AcnSr6_g";
+
 const Video = () => {
 
     return (
         <div className='video-wrapper' style={{}}>
+            {/*
             <Ratio aspectRatio="16x9">
                 <video src={src} type="video/mp4" autoPlay loop muted/>
             </Ratio>
+            */}
+            <ReactPlayer
+            url={myVideo}
+            controls={false}
+            playing={true}
+            loop={true}
+            volume={null}
+            muted={true}
+            width={'100%'}
+            height={'100%'}
+            />
         </div>
     )
 
